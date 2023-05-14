f17da8879c66b7d9c274e950907e358570a205b0
IvanKuzin <ik@e2xlabs.com>
2023-05-14 23:30:40 +0300

    add

--- nft-assets/nfts-metadata/build.bash
+++ nft-assets/nfts-metadata/build.bash
@@ -1,12 +1,7 @@
-#!/bin/bash
-filename="data/208.json"
-search="DogX — new forms of life on your planet. 3555 individuals are going to your planet. Don't worry, maybe we'll give you a game, or maybe new technology... It's up to you puppies."
-replace="DogX — new forms of life on your planet. 3555 individuals are going to your planet. Don't worry, maybe we'll give you a game, or maybe new technology... It's up to you puppies."
-dir="data"
-for entry in `ls ./$data`/*;
-do
-  if [[ $search != "" && $replace != "" ]]; then
-    sed -i'.bac' "s/${search//\//\/}/${replace//\//\/}/g" $entry
-    rm "$entry.bac"
-  fi
+search="search"
+replace="replace"
+
+for entry in `ls ./data/*; do
+  sed -i'.bac' "s/${search//\//\/}/${replace//\//\/}/g" $entry
+  rm "$entry.bac"
 done
