f2192ed14f50d258b36f692cb5410332c85d1b3c
IvanKuzin <ik@e2xlabs.com>
2024-05-14 20:05:17 +0300

    save

--- parse-files/src/images.ts
+++ parse-files/src/images.ts
@@ -1,12 +1,15 @@
 import * as fs from "fs";
 
 async function main() {
-    const folder = '../nft-assets/nfts/';
+    const folder = '../DogXCompleted/images/';
     const template = "DogX #";
 
     fs.readdir(folder, (err, files) => {
         files.filter(it => it.startsWith(template)).forEach(name => {
-            fs.rename(`${folder}/${name}`, `${folder}/${name.split(template)[1]}`, function(err) {
+
+            const index = +name.split(template)[1].split(".png")[0]
+
+            fs.rename(`${folder}/${name}`, `${folder}/${index - 1}.png`, function(err) {
                 if (err) console.log('ERROR: ' + err);
             });
         })
